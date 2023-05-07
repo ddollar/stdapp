0e189800f5c780fc66436408c2efa5b86e7fbc65
David Dollar <david@dollar.io>
2023-05-06 20:18:48 -0400

    init

---
+++ tools.go
@@ -0,0 +1,10 @@
+//go:build tools
+
+package convox
+
+import (
+	_ "github.com/cespare/reflex"
+	_ "github.com/ddollar/stdapp"
+	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
+	_ "github.com/goware/modvendor"
+)
