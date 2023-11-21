aef772a7d0deb1d0530bd4fb20ebe7ea17b6a621
David Dollar <david@dollar.io>
2023-11-21 07:32:19 -0500

    more typescript

---
+++ web/components/Alert.vue.d.ts
@@ -0,0 +1,14 @@
+declare const _default: import("vue").DefineComponent<{
+    error: {
+        type: ErrorConstructor;
+        default: null;
+    };
+}, {}, unknown, {}, {}, import("vue").ComponentOptionsMixin, import("vue").ComponentOptionsMixin, {}, string, import("vue").VNodeProps & import("vue").AllowedComponentProps & import("vue").ComponentCustomProps, Readonly<import("vue").ExtractPropTypes<{
+    error: {
+        type: ErrorConstructor;
+        default: null;
+    };
+}>>, {
+    error: Error;
+}, {}>;
+export default _default;
---
+++ web/format.d.ts
@@ -0,0 +1,4 @@
+export declare function day(date: any): any;
+export declare function firstName(name: any): any;
+export declare function money(amount: any): string;
+export declare function proper(name: any): any;
